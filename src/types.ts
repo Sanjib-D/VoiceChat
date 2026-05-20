30d2de805117e919821c521258dbe2affb9d8757
Sanjib Das <117588174+Sanjib-D@users.noreply.github.com>
2026-05-20 11:39:55 +0000

    deletion

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export type CallState = "idle" | "ringing" | "calling" | "connected";
-
-export interface SignalData {
-  type: 'offer' | 'answer' | 'ice-candidate' | 'reject';
-  from: string;
-  signal?: any;
-  callerName?: string;
-}
